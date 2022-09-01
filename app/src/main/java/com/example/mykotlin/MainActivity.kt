5c860c39e828563605e11c7bdd4dffadacef0a18
whiteboolean@gmail.com <whiteboolean@gmail.com>
2022-09-01 23:26:19 +0800

    协程

--- app/src/main/java/com/example/mykotlin/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.mykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logE("1111")
+        GlobalScope.launch {
+            logE("2222")
+            getFirst()
+            logE("3333")
+            getSecond()
+            logE("4444")
+        }
+        logE("5555")
+    }
+
+
+
+    private fun getFirst(){
+        Thread.sleep(2000)
+    }
+
+    private fun getSecond(){
+        Thread.sleep(3000)
+    }
+    
+    val logE: (String)->Unit = {
+        Log.e("MainActivity",it)
     }
 }
